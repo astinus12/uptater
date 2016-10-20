1c8c3d70751b9a3c34dc4124428f36811f835dba
Will <wnovak@Williams-MacBook-Pro.local>
2016-10-20 15:29:23 -0400

    Update from testater to uptater 1

--- uptater.sh
+++ uptater.sh
@@ -12,13 +12,8 @@ echo ${bold}"Updating oh-my-zsh..."${normal}
 sh $ZSH/tools/upgrade.sh
 echo
 echo ${bold}"Updating git repos..."${normal}
-echo fabric:
-cd /Users/wnovak/Documents/git/fabric; git pull & wait
-echo serverspec:
-cd /Users/wnovak/Documents/git/serverspec; git pull & wait
-echo bac:
-cd /Users/wnovak/Documents/git/bac; git pull & wait
-echo uptater:
+cd /Users/wnovak/Documents/git
+for REPO in `ls`; do (cd "$REPO"; echo "updating" ${REPO} git pull); done;
 cd /Users/wnovak/Documents/Scripts/git/uptater; git pull & wait
 echo
 echo ${bold}"Updating svn repos..."${normal}
